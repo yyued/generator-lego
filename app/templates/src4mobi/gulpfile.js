f4977477e583043ff50efdaf658874cac72f3443
lilijialiang <353943780@qq.com>
2015-05-11 10:33:32 +0800

    update mobile

--- app/templates/src4mobi/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-	plugins = require('gulp-load-plugins')();
-
-var a = 'jl'
-
-require('./tasks/gulpfile-dev')(gulp, plugins)
-require('./tasks/gulpfile-build')(gulp, plugins)
-require('./tasks/gulpfile-zip')(gulp, plugins)
